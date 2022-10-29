a602a2370ef16756a123b1d613f8a343e2febef3
clarity248 <109438085+clarity248@users.noreply.github.com>
2022-10-29 16:38:25 -0400

    Recreate M2 Turn Fixes #15

--- m3turn.py
+++ m3turn.py
@@ -3,7 +3,6 @@
 from spike import PrimeHub, LightMatrix, Button, StatusLight, ForceSensor, MotionSensor, Speaker, ColorSensor, App, DistanceSensor, Motor, MotorPair
 from spike.control import wait_for_seconds, Timer, wait_until
 from spike.operator import *
-import hub
 from math import *
 
 #Initialization
@@ -45,7 +44,7 @@ def findColor(targetColor, colorSensor, leftMotorSpeed, rightMotorSpeed):
 def startMission():
     # Resets all values for each motor and the gyro sensor.
     megaBotsPrime.right_button.wait_until_pressed()
-    wait_for_seconds(1)
+    wait_for_seconds(2)
     leftMotor.set_degrees_counted(0)
     rightMotor.set_degrees_counted(0)
     frontMotor.set_degrees_counted(0)
@@ -66,100 +65,10 @@ def m3Turn(targetGyro, offsetGyro, pauseTime, leftMotorSpeed, rightMotorSpeed):
     wait_until(gyroNormalize, equal_to, targetGyro)
     motorPair.stop()
 
-def showBatteryLevel():
-    megaBotsPrime.light_matrix.write(hub.battery.capacity_left())
-
-showBatteryLevel()
 startMission()
 
-#Turn towards hydroelectric dam, 1st turn
-m3Turn(40, 0, 0, 20, 0)
+motorPair.move_tank(50, "degrees", -10, 10)
 
-#Move towards dam
-motorPair.move_tank(250, "degrees", 30, 30)
 
-#whip the water unit
-m3Turn(335, 0, 0, -40, 60)
-
-#Turn left toward North, 2nd turn
-#m3Turn(2, 0, 0, 0, 20)
-
-m3Turn(2, 0, 0, 0, -25)
-
-findColor(blackThreshold, rightColor, 20, 20)
-
-#Turn East, 3rd turn
-m3Turn(90, 0, 0, 20, 0)
-
-findColor(blackThreshold, rightColor, 20, 20)
-
-motorPair.move_tank(50, "degrees", 30, 30)
-
-#Turn South, 4th turn
-m3Turn(179, 0, 0, 20, 0)
-
-#Move back towards Smart Grid
-motorPair.move_tank(200, "degrees", -20, -20)
-
-#Drop the back hook
-backMotor.run_for_degrees(160,70)
-
-#Pull Smart Grid lever
-motorPair.move_tank(100, "degrees", 10, 10)
-
-#Raise the back hook
-backMotor.run_for_degrees(160,-50)
-
-#Move forward to the hydrogen plant
-motorPair.move_tank(150, "degrees", 20, 20)
-
-#Turn N/NW toward Solar Farm, 5th turn
-m3Turn(335, 0, 0, -10, 10)
-
-#Go towards Solar Farm
-motorPair.move_tank(260, "degrees", 30, 30)
-
-#Slow curve turn to collect first energy unit, 6th turn
-m3Turn(275, 0, 0, 13, 20)
-
-#Collect last two energy units
-#motorPair.move_tank(50, "degrees", 30, 30)
-
-#Turn to SouthWest, 7th turn
-m3Turn(220, 0, 0, -10, 10)
-
-#Find the black line to line up with oil rig lever
-findColor(blackThreshold, rightColor, 20, 20)
-
-#
-motorPair.move_tank(160, "degrees", 20, 20)
-
-m3Turn(263, 0, 0, 10, -10)
-
-#First raise of Oil Platform lever
-motorPair.move_tank(100, "degrees", 15, 15)
-
-motorPair.move_tank(80, "degrees", -15, -15)
-
-motorPair.move_tank(80, "degrees", 15, 15)
-
-motorPair.move_tank(80, "degrees", -15, -15)
-
-motorPair.move_tank(80, "degrees", 15, 15)
-
-motorPair.move_tank(70, "degrees", -15, -15)
-
-#Turn towards Red Base
-m3Turn(215, 0, 0, 0, 20)
-
-motorPair.move_tank(500, "degrees", 40, 40)
-
-motorPair.move_tank(600, "degrees", -40, -40)
-
-m3Turn(182, 0, 0, -20, 0)
-
-m3Turn(225, 0, 0, 20, 15)
-
-motorPair.move_tank(550, "degrees", 40, 40)
-
-showBatteryLevel()
+print(gyroNormalize())
+print(gyroSensor.get_yaw_angle())
