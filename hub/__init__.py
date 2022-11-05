d9020bece762dbd8164ad55f447f4f1898ab0980
clarity248 <109438085+clarity248@users.noreply.github.com>
2022-11-04 22:19:24 -0400

    Mission Run #1 Fixes #18

---
+++ hub/__init__.py
@@ -0,0 +1,17 @@
+"""
+Hub Python Classes
+
+Needed to prevent pylance errors for battery reporting
+"""
+class battery:
+    def __init__(self):
+        pass
+    def capacity_left(self):
+        """
+        Returns the current remaining battery capacity as an integer form of a percentage
+        
+        Parameters
+        -------------
+        None
+        """
+        pass
